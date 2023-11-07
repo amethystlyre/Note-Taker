f90abda2a69874d5d613d05ca6bf7cece5daa8b7
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-07 21:29:55 +1100

    update front end template code to the latest version

--- routes/notes.js
+++ routes/notes.js
@@ -1,63 +1,63 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readDB, writeDB} = require('../helper/accessDB.js');
+const { readDB, writeDB } = require('../helper/accessDB.js');
 
 const DB_LOCATION = "./db/db.json";
 
 // GET Route for retrieving all notes
 notes.get('/', (req, res) => {
-    readDB(DB_LOCATION
-    ).then((data) => res.json(JSON.parse(data)));
-  });
+  readDB(DB_LOCATION)
+    .then((data) => res.json(JSON.parse(data)));
+});
 
 // GET Route for retrieving a specific note based on note id
 notes.get('/:id', (req, res) => {
-    const noteId = req.params.id;
+  const noteId = req.params.id;
 
-    readDB(DB_LOCATION)
+  readDB(DB_LOCATION)
     .then((data) => JSON.parse(data))
     .then((json) => {
       const result = json.filter((note) => note.id === noteId);
-          return result.length > 0
-          ? res.json(result)
-          : res.status(400).json('No note with that ID');
+      return result.length > 0
+        ? res.json(result)
+        : res.status(400).json('No note with that ID');
     });
-  });
+});
 
 // POST Route for saving a new note
 notes.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text} = req.body;
-  
-    if (title && text) {
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
+  console.log(req.body);
 
-      readDB(DB_LOCATION)
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readDB(DB_LOCATION)
       .then((data) => JSON.parse(data))
-      .then((json) =>{
+      .then((json) => {
         json.push(newNote);
-        writeDB(DB_LOCATION,json);
+        writeDB(DB_LOCATION, json);
       })
 
-      res.json(`Note added successfully`);
-    } else {
-      res.status(400).json('Error in adding note');
-    }
-    
-  });
+    res.json(`Note added successfully`);
+  } else {
+    res.status(400).json('Error in adding note');
+  }
+
+});
 
 // DELETE Route for removing a note in the DB based on given id
 notes.delete('/:id', (req, res) => {
-    const noteId = req.params.id;
+  const noteId = req.params.id;
 
-    readDB(DB_LOCATION)
+  readDB(DB_LOCATION)
     .then((data) => JSON.parse(data))
-    .then((json) =>{
+    .then((json) => {
       //remove the note with the given id from the json DB
       const result = json.filter((note) => note.id !== noteId);
 
@@ -68,7 +68,7 @@ notes.delete('/:id', (req, res) => {
       res.json(`Item ${noteId} has been deleted.`);
 
     });
-    
-  });
-  
-  module.exports = notes;
+
+});
+
+module.exports = notes;
